dbfb140d853cd074bc4e3b40cf5619dd65b2a845
Ivanov Ihor <i.ivanovvv21122006@gmail.com>
2026-02-14 15:37:05 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,7 +19,17 @@ promise1.then(() => {
 });
 
 const promise2 = new Promise((resolve, reject) => {
-    setTimeout(reject, 3000);
+  setTimeout(reject, 3000);
+});
+
+promise1.catch(() => {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.classList.add('error-message');
+  div.textContent = `Promise was rejected!`;
+
+  document.body.appendChild(div);
 });
 
 promise2.catch(() => {
@@ -29,5 +39,13 @@ promise2.catch(() => {
   div.classList.add('error-message');
   div.textContent = `Promise was rejected!`;
 
+  document.body.appendChild(div);
+});
+
+promise2.then(() => {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = `Promise was resolved!`;
   document.body.appendChild(div);
 });
